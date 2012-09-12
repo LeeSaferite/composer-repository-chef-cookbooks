451ba04a3ae75a78f86f7b5982406a7f8d660213
Daniel Howlett <dan@dancras.co.uk>
2012-09-12 01:48:58 +0100

    Add script, repository and readme docs

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-composer-repository-chef-cookbooks
-==================================
+Composer Repository for Chef Cookbooks
+======================================
 
-A packages.json repository for composer, and script to generate it
+Generated from the opscode-cookbooks organisation using the github api.
+
+Usage example:
+
+    {
+        "minimum-stability": "dev",
+        "require": {
+            "opscode-cookbooks/apache2": "1.1.*"
+        }
+	"repositories": [
+            {
+                    "type": "composer",
+                    "url": "https://raw.github.com/dancras/composer-repository-chef-cookbooks/master"
+            }
+        ]
+    }
+
+Dev stability is required for the composer cookbook installer and will be fixed soon.
+
---
+++ create_repository.php
@@ -0,0 +1,56 @@
+<?php
+
+function prepare_version($ref) {
+
+    if (strpos($ref, 'refs/heads') === false && strpos($ref, 'refs/tags') === false) {
+        return null;
+    }
+
+    $refName = str_replace('refs/heads/', '', str_replace('refs/tags/', '', $ref));
+
+    if (preg_match('/\d+\.\d+\.\d+\w{0,20}/', $refName)) {
+        return $refName;
+    }
+
+    return 'dev-' . $refName;
+}
+
+function prepare_ref($ref) {
+    return str_replace('refs/heads/', '', str_replace('refs/tags/', '', $ref));
+}
+
+$repositories = json_decode(file_get_contents('https://api.github.com/orgs/opscode-cookbooks/repos'));
+
+foreach ($repositories as $repository) {
+
+    $refs = json_decode(file_get_contents("https://api.github.com/repos/{$repository->full_name}/git/refs"));
+
+    $versions = array();
+
+    foreach ($refs as $ref) {
+
+        $version = prepare_version($ref->ref);
+
+        if (!$version) {
+            continue;
+        }
+
+        $versions[$version] = array(
+            'name' => $repository->full_name,
+            'type' => 'chef-cookbook',
+            'version' => $version,
+            'source' => array(
+                'type' => 'git',
+                'url' => $repository->git_url,
+                'reference' => prepare_ref($ref->ref)
+            ),
+            'require' => array(
+                'dancras/chef-cookbook-installer' => '*'
+            )
+        );
+    }
+
+    $packages[$repository->full_name] = $versions;
+}
+
+echo str_replace('\\/', '/', json_encode($packages));
